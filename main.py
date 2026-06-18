f87ad36e0335dcc241c216934c7a4709ac2709c6
Victor Hugo <victorhugo81@yahoo.com>
2026-06-17 18:29:37 -0700

    fix the export button

--- main.py
+++ main.py
@@ -104,10 +104,10 @@ def create_app(config_name=None):
         response.headers['Permissions-Policy'] = 'geolocation=(), microphone=(), camera=()'
         response.headers['Content-Security-Policy'] = (
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline'; "
+            "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; "
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
             "font-src 'self' https://fonts.gstatic.com; "
-            "img-src 'self' data:; "
+            "img-src 'self' data: blob:; "
             "connect-src 'self'; "
             "frame-ancestors 'self';"
         )
